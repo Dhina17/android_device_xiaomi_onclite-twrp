4607530ed314671ddb12c361aa38f900d81e2558
Dhina17 <dhinalogu@gmail.com>
2020-04-01 21:49:44 +0000

    Set TW_NO_USB_STORAGE to true
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,6 +99,7 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 TW_HAS_EDL_MODE := true
+TW_NO_USB_STORAGE := true
 
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
